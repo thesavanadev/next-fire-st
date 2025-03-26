84852f9ac3d386aebabb1e5cb66e9923ad2c7704
Michael Owuor <michael.owuor@gmail.com>
2025-03-26 23:24:30 +0300

    feat: add initial project setup with Tailwind CSS, PostCSS, and utility components

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,30 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+	{
+		rules: {
+			"@typescript-eslint/ban-ts-comment": "warn",
+			"@typescript-eslint/no-empty-object-type": "warn",
+			"@typescript-eslint/no-explicit-any": "warn",
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{
+					vars: "all",
+					args: "after-used",
+					ignoreRestSiblings: false,
+					argsIgnorePattern: "^_",
+					varsIgnorePattern: "^_",
+					destructuredArrayIgnorePattern: "^_",
+					caughtErrorsIgnorePattern: "^(_|ignore)",
+				},
+			],
+		},
+	},
 ];
 
 export default eslintConfig;
